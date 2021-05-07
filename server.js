fa854575ac92aa91d568a3f77b09f70f38561c1e
Lucas Coutinho <lucaspp1@ucl.br>
2021-05-07 14:59:45 -0300

    add port env

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors()); // libera acesso para todos domínios. 
-// preencher com os parâmetros, restringe aos especificados
-//rotas
+const PORT = process.env.PORT || 3000
 app.use("/", require("./src/routes"));
-app.listen(3000);
+app.listen(PORT);
